0469bd15922cb397cbf571e48e1eb0c8212ca8cf
Thomas Boerger <thomas@webhippie.de>
2025-03-27 19:59:19 +0100

    feat: apply new repo structure and workflows

--- .github/workflow-issue.md
+++
@@ -1,8 +0,0 @@
----
-title: Unexpected {{ workflow }} failure
-assignees: tboerger
-labels: bug
----
-
-The automatic {{ workflow }} workflow failed, please investigate:
-{{ env.GITHUB_SERVER_URL }}/{{ env.GITHUB_REPOSITORY }}/actions/runs/{{ env.GITHUB_RUN_ID }}/attempts/{{ env.GITHUB_RUN_ATTEMPT }}
--- flake.nix
+++ flake.nix
@@ -3,35 +3,90 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    utils = {
-      url = "github:numtide/flake-utils";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
     };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            _1password
-            chart-testing
-            go
-            helm-docs
-            kind
-            kubeconform
-            kubernetes-helm
-            yamllint
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          imports = [
+            {
+              _module.args.pkgs = import inputs.nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            }
           ];
+
+          devenv = {
+            shells = {
+              default = {
+                git-hooks = {
+                  hooks = {
+                    nixfmt-rfc-style = {
+                      enable = true;
+                    };
+
+                    gofmt = {
+                      enable = true;
+                    };
+                  };
+                };
+
+                languages = {
+                  go = {
+                    enable = true;
+                    package = pkgs.go_1_24;
+                  };
+                };
+
+                packages = with pkgs; [
+                  _1password-cli
+                  chart-testing
+                  helm-docs
+                  kind
+                  kubeconform
+                  kubernetes-helm
+                  nixfmt-rfc-style
+                  yamllint
+                ];
+              };
+            };
+          };
         };
-      }
-    );
+    };
 }
